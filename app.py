d58dc9c42f816118b8fb3add43b22f273da5dafa
Tony Doan <ynotdoan@gmail.com>
2020-12-27 15:24:22 -0800

    put all frames in functions

--- app.py
+++ app.py
@@ -4,9 +4,11 @@ import time
 import random_gen as r
 
 
-d_font = ("times", 40, "bold")
+b_font = ("times", 40, "bold")
+s_font = ("times", 12, "bold")
 colours = ["red", "orange", "yellow", "green", "blue", "purple", 
            "black", "white", "pink", "brown", "grey"]
+
 
 def get_colours():
   # makes variables global to be accessible by rest of program
@@ -22,89 +24,117 @@ def get_colours():
     if (bg_colour != txt_colour):
       break
 
+
 def show_start():
+  start_frame = tk.Frame(wn, background = "gold")
+  # game title
+  tk.Label(start_frame, 
+          text = "Speed Colour\nGame", 
+          background = "gold", 
+          foreground = "black", 
+          font = b_font, 
+          ).pack(pady = 30)
+  # instructions label 
+  tk.Label(start_frame, 
+          text = "Try to type the colour of the word on-screen quickly!\n \
+                  \nClick 'Start' to begin.", 
+          background = "gold", 
+          foreground = "black", 
+          font = s_font, 
+          ).pack(anchor = "center")
+  # start button
+  tk.Button(start_frame, 
+            text = "Start!", 
+            width = 30, 
+            font = s_font, 
+            command = lambda: show_game(start_frame), 
+            ).pack(anchor = "center", expand = True)
+  
   start_frame.pack(fill = "both", expand = True)
-  
+
+
 def show_game(frame):
-  frame.pack_forget()
-  game_frame.pack(fill = "both", expand = True)
+  frame.destroy()
+  get_colours()
   
-def show_result():
-  game_frame.pack_forget()
+  global game_frame, start_time
+  
+  game_frame = tk.Frame(wn, background = colours[bg_colour])
+  # colour text label
+  tk.Label(game_frame, 
+          text = colours[txt], 
+          background = colours[bg_colour], 
+          foreground = colours[txt_colour], 
+          font = b_font, 
+          ).pack(pady = 70)
+  # entry instructions
+  tk.Label(game_frame, 
+          text = "Click 'Enter' to check", 
+          background = colours[bg_colour], 
+          foreground = colours[txt_colour], 
+          font = s_font, 
+          ).pack(anchor = "center")
+  # entry box
+  entry = tk.Entry(game_frame, 
+                  width = 30, 
+                  borderwidth = 5, 
+                  )
+  entry.pack(anchor = "center", expand = True)
+
+  game_frame.pack(fill = "both", expand = True)
+  # starts timer after game_frame is shown
+  start_time = time.perf_counter()
+  entry.bind("<Return>", lambda event: check_entry(entry.get()))
+
+
+def show_result(frame):
+  frame.destroy()
+  
+  result_frame = tk.Frame(wn, background = "green")
+  # congrats message
+  tk.Label(result_frame, 
+          text = f"Correct!! {time_diff:.2f} s", 
+          background = "green", 
+          foreground = "black", 
+          font = b_font, 
+          ).pack(anchor = "center", expand = True)
+  # more labels here...
+  # quit button
+  tk.Button(result_frame, 
+            text = "EXIT", 
+            background = "grey", 
+            foreground = "white", 
+            width = 20, 
+            font = s_font, 
+            command = quit
+            ).pack(anchor = "center", expand = True)
+  
   result_frame.pack(fill = "both", expand = True)
+  result_frame.focus_set()
+  result_frame.bind("<Return>", lambda event: show_game(result_frame))
   
 def check_entry(entry):
+  global time_diff
+  
   if (entry == colours[txt_colour]):
-    show_result()
+    # gets current time when guess is correct and finds time difference
+    time_diff = time.perf_counter() - start_time
+    show_result(game_frame)
   elif (entry != colours[txt_colour]):
-    print ("OOF")
+    # displays incorrect message 
+    incorrect = tk.Label(game_frame, 
+                         text = "Incorrect! Try again...", 
+                         background = colours[bg_colour], 
+                         foreground = colours[txt_colour], 
+                         font = s_font, 
+                         )
+    incorrect.pack(anchor = "center", side = "bottom"), 
+    incorrect.after(1000, incorrect.destroy())
     
 
 wn = tk.Tk()
 wn.title("Colour Guessing Game")
 wn.minsize(400, 400) # widthxheight
-
-get_colours()
-start_frame = tk.Frame(wn, background = "gold")
-# game title
-tk.Label(start_frame, 
-         text = "Speed Colour\nGame", 
-         background = "gold", 
-         foreground = "black", 
-         font = d_font, 
-         ).pack(pady = 30)
-# instructions label 
-tk.Label(start_frame, 
-         text = "Try to type the colour of the word on-screen quickly!\n \
-                 \nClick 'Start' to begin.", 
-         background = "gold", 
-         foreground = "black", 
-         font = ("times", 12)
-         ).pack(anchor = "center")
-# start button
-tk.Button(start_frame, 
-          text = "Start!", 
-          width = 30,  
-          command = lambda: show_game(start_frame), 
-          ).pack(anchor = "center", expand = True)
-
-
-game_frame = tk.Frame(wn, background = colours[bg_colour])
-# colour text label
-tk.Label(game_frame, 
-         text = colours[txt], 
-         background = colours[bg_colour], 
-         foreground = colours[txt_colour], 
-         font = d_font, 
-         ).pack(pady = 70)
-# entry instructions
-tk.Label(game_frame, 
-         text = "Click 'Enter' to check", 
-         background = colours[bg_colour], 
-         foreground = colours[txt_colour], 
-         font = ("times", 12), 
-         ).pack(anchor = "center")
-# entry box
-entry = tk.Entry(game_frame, 
-                 width = 30, 
-                 borderwidth = 5, 
-                 )
-entry.pack(anchor = "center", expand = True)
-entry.bind("<Return>", lambda event: check_entry(entry.get()))
-
-
-result_frame = tk.Frame(wn, background = "green")
-# congrats message
-tk.Label(result_frame, 
-         text = "Correct!! ", 
-         background = "green", 
-         foreground = "black", 
-         font = d_font, 
-         ).pack(anchor = "center")
-# press key to continue
-result_frame.focus_set()
-result_frame.bind("<Return>", lambda event: show_game(result_frame))
-
 
 show_start()
 wn.mainloop()
